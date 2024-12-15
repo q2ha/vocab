6c0c4924a07ff4da9ba920d9e63244edc12bdf0b
q2ha <94222718+q2ha@users.noreply.github.com>
2024-12-15 15:17:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function speakWord(word) {
+    const words = word.split(' ');  // فصل الكلمات إذا كان هناك أكثر من كلمة واحدة
+    let index = 0;
+    
+    function speakNext() {
+      if (index < words.length) {
+        const utterance = new SpeechSynthesisUtterance(words[index]);
+        utterance.lang = 'en-GB'; // تغيير اللغة إلى الإنجليزية البريطانية
+        speechSynthesis.speak(utterance);
+        
+        index++;
+        setTimeout(speakNext, 3000);  // الانتظار لمدة 3 ثواني قبل نطق الكلمة التالية
+      }
+    }
+  
+    speakNext(); // بدء الكلام
+  }
+  
