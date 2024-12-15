879143c08431ea1b4921a3a1aa56a3713c81ed5a
q2ha <94222718+q2ha@users.noreply.github.com>
2024-12-15 15:28:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function speakWord(word) {
     const utterance = new SpeechSynthesisUtterance(word);
     utterance.lang = 'en-US'; // يمكنك تغيير اللغة هنا إذا كنت تستخدم كلمات باللغة العربية
+    utterance.rate = 0.5; // هذه القيمة هي التي ستبطئ النطق، يمكنك تعديلها حسب الحاجة
     speechSynthesis.speak(utterance);
   }
-  
