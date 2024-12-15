87480781867d84b2e3d9c871504e96f10eb0b630
q2ha <94222718+q2ha@users.noreply.github.com>
2024-12-15 15:23:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
 function speakWord(word) {
-    const words = word.split(' ');  // فصل الكلمات إذا كان هناك أكثر من كلمة واحدة
-    let index = 0;
-    
-    function speakNext() {
-      if (index < words.length) {
-        const utterance = new SpeechSynthesisUtterance(words[index]);
-        utterance.lang = 'en-US'; // تغيير اللغة إلى الإنجليزية البريطانية
-        speechSynthesis.speak(utterance);
-        
-        index++;
-        setTimeout(speakNext, 3000);  // الانتظار لمدة 3 ثواني قبل نطق الكلمة التالية
-      }
-    }
-  
-    speakNext(); // بدء الكلام
+    const utterance = new SpeechSynthesisUtterance(word);
+    utterance.lang = 'en-US'; // يمكنك تغيير اللغة هنا إذا كنت تستخدم كلمات باللغة العربية
+    speechSynthesis.speak(utterance);
   }
   
